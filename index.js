07628f26f1c3b9356739f5bca9920548b105d9c4
Peace <bunmipeace@yahoo.com>
2022-02-21 18:33:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let num = prompt("Your weight in kg")
+console.log(document.getElementById("size").innerHTML = num)
+
+result = num * 3.280839895
+document.getElementById("met-fet").innerHTML = num + " meters = " + result + " feet"
+result2 = num * 0.2641720524
+document.getElementById().innerHTML = num + " liters = " + result2 + " gallon"
+result3 
